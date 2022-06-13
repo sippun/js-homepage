b41e584350e963a64d9118ad5fe417bb9b409ce7
Joel <sippun@users.noreply.github.com>
2022-06-12 18:24:45 -0700

    Fix particles bg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import {
   ChakraProvider,
   Box,
@@ -9,7 +9,7 @@ import { useLocation } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
 import Particles from 'react-tsparticles';
 import { loadFull } from 'tsparticles';
-import particlesConfig from './theme/particlesConfig';
+import particlesConfig from './theme/particlesjs-config.json';
 import { AnimatePresence } from 'framer-motion';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
@@ -19,24 +19,14 @@ import Books from './pages/Books';
 function App() {
   const location = useLocation();
 
-  const particlesInit = async (main) => {
-    console.log(main);
-    await loadFull(main);
-  };
-
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
+  const particlesInit = useCallback(main => {
+    loadFull(main);
+  }, [])
 
   return (
     <ChakraProvider theme={theme}>
       <Box>
-      <Particles
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      options={particlesConfig}
-    />
+        <Particles options={particlesConfig} init={particlesInit}/>
 
         <Navbar />
 
