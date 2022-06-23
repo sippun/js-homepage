ff9a067b86da9f062ae1f401aeb5117f80878e8c
Joel <sippun@users.noreply.github.com>
2022-06-22 17:55:59 -0700

    Edit contact buttons

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,26 +1,58 @@
-import { Button, Container, Center, Link } from '@chakra-ui/react';
+import {
+  Button,
+  Container,
+  Center,
+  Link,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverBody,
+  PopoverArrow,
+  VStack,
+  HStack,
+  Heading,
+} from '@chakra-ui/react';
+import { CopyIcon, EmailIcon } from '@chakra-ui/icons';
 import { LayoutTransitions } from '../theme/Transitions';
 
 const Home = () => {
 
   const copyDiscord = () => {
     navigator.clipboard.writeText("sippy#8480").then(() => {
-      // Alert the user that the action took place.
-      // Nobody likes hidden stuff being done under the hood!
-      alert("Copied sippy#8480 to clipboard");
+      //alert("Copied sippy#8480 to clipboard");
   });
   }
   return (
     <LayoutTransitions>
       <Container h="calc(100vh - 64px)" maxW="container.md" p={0}>
         <Center h="calc((100vh - 64px) * 0.8)">
-          Get in touch
-          <Button as={Link} href="mailto:joel.s.sheng@gmail.com">
-            Email
-          </Button>
-          <Button onClick={copyDiscord}>
-            Discord
-          </Button>
+          <VStack>
+            <Heading>Get in touch</Heading>
+              <Button
+                as={Link}
+                href="mailto:joel.s.sheng@gmail.com"
+                rightIcon={<EmailIcon />}
+              >
+                Email: joel.s.sheng@gmail.com
+              </Button>
+
+              <Popover placement="top">
+                <PopoverTrigger>
+                  <Button
+                    rightIcon={<CopyIcon />}
+                    onClick={copyDiscord}
+                  >
+                    Discord: sippy#8480
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent bg="cleargrey" border="none">
+                  <PopoverArrow />
+                  <PopoverBody>
+                    Copied sippy#8480 to clipboard
+                  </PopoverBody>
+                </PopoverContent>
+              </Popover>
+          </VStack>
         </Center>
       </Container>
     </LayoutTransitions>
