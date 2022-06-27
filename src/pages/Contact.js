39a10ab300caf1f22c56c18e97c6e7af0d10f1a3
Joel <sippun@users.noreply.github.com>
2022-06-26 17:17:33 -0700

    Use Toast instead of Popover in Contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,60 +3,64 @@ import {
   Container,
   Center,
   Link,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  PopoverBody,
-  PopoverArrow,
   VStack,
   HStack,
   Heading,
   Text,
+  useToast,
 } from '@chakra-ui/react';
 import { CopyIcon, EmailIcon } from '@chakra-ui/icons';
 import { LayoutTransitions } from '../theme/Transitions';
 
 const Home = () => {
+  const toast = useToast();
 
-  const copyDiscord = () => {
-    navigator.clipboard.writeText("sippy#8480").then(() => {
-      //alert("Copied sippy#8480 to clipboard");
-  });
+  const copy = (option) => {
+    var text = "";
+    if (option === "email") text = "joel.s.sheng@gmail.com";
+    if (option === "discord") text = "sippy#8480";
+    navigator.clipboard.writeText(text).then(() => {
+      toast({
+        title: "Copied '" + {text} + "' to clipboard",
+        status: "success",
+        variant: "subtle",
+        duration: 2000,
+      })
+    });
   }
+
   return (
     <LayoutTransitions>
       <Container h="calc(100vh - 64px)" maxW="container.md" p={0}>
         <Center h="calc((100vh - 64px) * 0.8)">
-          <VStack>
-            <Heading>Get in touch</Heading>
-            <HStack>
-              <Text>Email:</Text>
-              <Button
-                as={Link}
-                href="mailto:joel.s.sheng@gmail.com"
-                rightIcon={<EmailIcon />}
-              >
-                joel.s.sheng@gmail.com
-              </Button>
+          <VStack w="full">
+            <Heading variant="mono">Get in touch</Heading>
+            <HStack justifyContent="space-between">
+              <Text alignSelf="start" mt={2}>Email:</Text>
+              <VStack>
+                <Button
+                  rightIcon={<CopyIcon />}
+                  onClick={copy("email")}
+                >
+                  joel.s.sheng@gmail.com
+                </Button>
+                <Button
+                  as={Link}
+                  href="mailto:joel.s.sheng@gmail.com"
+                  rightIcon={<EmailIcon />}
+                >
+                  Send email
+                </Button>
+              </VStack>
             </HStack>
-            <HStack>
+            <HStack justifyContent="space-between">
               <Text>Discord: </Text>
-              <Popover placement="bottom">
-                <PopoverTrigger>
-                  <Button
-                    rightIcon={<CopyIcon />}
-                    onClick={copyDiscord}
-                  >
-                    sippy#8480
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent bg="cleargrey" border="none">
-                  <PopoverArrow />
-                  <PopoverBody>
-                    Copied sippy#8480 to clipboard
-                  </PopoverBody>
-                </PopoverContent>
-              </Popover>
+              <Button
+                rightIcon={<CopyIcon />}
+                onClick={copy("discord")}
+              >
+                sippy#8480
+              </Button>
             </HStack>
           </VStack>
         </Center>
