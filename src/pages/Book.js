166ff8de95b037893f0bb40fffc3ccf63fd41818
Joel <sippun@users.noreply.github.com>
2022-07-13 17:22:24 -0700

    Add book image and improve layout of book page

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -6,8 +6,10 @@ import {
   Container,
   Heading,
   HStack,
+  Image,
   Link,
   Text,
+  VStack,
 } from '@chakra-ui/react';
 import { ChevronLeftIcon, ExternalLinkIcon } from '@chakra-ui/icons';
 import { LayoutTransitions } from '../theme/Transitions';
@@ -26,7 +28,7 @@ const Book = () => {
   useEffect(() => {
     axios.get(baseURL + GOOGLE_API_KEY).then((response) => {
       setBookInfo(response);
-      console.log(response.data);
+      //console.log(response.data);
     });
   }, [baseURL]);
 
@@ -37,55 +39,69 @@ const Book = () => {
   );
 
   const bookDescription = bookInfo.data.volumeInfo.description;
+  const bookImage = '/images/books/' + bookData.image;
 
   return (
     <LayoutTransitions>
       <Container h="calc(100vh - 64px)" maxW="container.xl" p={0} centerContent>
         <ContentBox h="calc((100vh - 64px) * 0.95)" maxW="container.md">
-          
-          <Link as={ReactLink} to="/readinglist">
-            <HStack spacing={0}>
-              <ChevronLeftIcon boxSize={6} color="accent.300"/>
-              <Heading
-                fontSize="2xl"
-                variant="code"
-                color="accent.300"
-                _hover={{ textDecoration: "underline"}}
-              >
-                Reading List
-              </Heading>
-            </HStack>
-          </Link>
-            
-            
-          <Box mt={3}>
-            <Heading fontSize="xl">{bookInfo.data.volumeInfo.title}</Heading>
-            <Heading fontSize="sm">{bookInfo.data.volumeInfo.subtitle}</Heading>
-            <Heading fontSize="md">By&nbsp;
-            {bookInfo.data.volumeInfo.authors.map((author, i, { length }) => {
-              if(length === 1) return <>{author}</>;
-              if(i < length - 1) return <>{author}, &nbsp;</>;
-              return <>{author}</>;
-            })}
-            </Heading>
-            <br/>
-            <Heading fontSize="lg">
-              Description
-            </Heading>
-            <Text fontSize="sm">
-              <Markup content={bookDescription} />
-            </Text>
-            <HStack w="full" justifyContent="flex-end">
-              
-                <a href={bookData.url} target="_blank" rel="noopener noreferrer">
-                  <Button 
-                    rightIcon={<ExternalLinkIcon />}
+          <VStack h="full" justifyContent="space-between">
+            <Box>
+              <Link as={ReactLink} to="/readinglist">
+                <HStack spacing={0}>
+                  <ChevronLeftIcon boxSize={6} color="accent.300"/>
+                  <Heading
+                    fontSize="2xl"
+                    variant="code"
+                    color="accent.300"
+                    _hover={{ textDecoration: "underline"}}
                   >
-                    Link
-                  </Button>
-                </a>
+                    Reading List
+                  </Heading>
+                </HStack>
+              </Link>
+              
+              <HStack alignItems="start" mt={3}>
+                
+                <Box w='200px' h='200px' bg='aqua' mb={1}>
+                  <Image
+                    w="200px"
+                    h="200px"
+                    objectFit="cover"
+                    objectPosition="top"
+                    src={bookImage}
+                  />
+                </Box>
+                <Box>
+                  <Heading fontSize="2xl" fontFamily="Inter">{bookInfo.data.volumeInfo.title}</Heading>
+                  <Heading fontSize="md">{bookInfo.data.volumeInfo.subtitle}</Heading>
+                  <Heading fontSize="md" fontFamily="Inter" mt={2}>By&nbsp;
+                  {bookInfo.data.volumeInfo.authors.map((author, i, { length }) => {
+                    if(length === 1) return <>{author}</>;
+                    if(i < length - 1) return <>{author}, &nbsp;</>;
+                    return <>{author}</>;
+                  })}
+                  </Heading>
+                </Box>
+              </HStack>
+              <Heading fontSize="xl" mt={3}>
+                Description
+              </Heading>
+              <Text fontSize="sm" mt={1}>
+                <Markup content={bookDescription} />
+              </Text>
+            </Box>
+            <HStack w="full" justifyContent="flex-end">
+                
+              <a href={bookData.url} target="_blank" rel="noopener noreferrer">
+                <Button 
+                  rightIcon={<ExternalLinkIcon />}
+                >
+                  Link
+                </Button>
+              </a>
             </HStack>
-          </Box>
+          </VStack>
         </ContentBox>
       </Container>
     </LayoutTransitions>
